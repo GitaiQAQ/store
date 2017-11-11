d75f2d10587a6d03ed19e3f037bebd5781942bf7
sivanZhang <445080336@qq.com>
2017-11-11 10:34:49 +0800

    回到顶部

--- static/products/js/detail.js
+++ static/products/js/detail.js
@@ -171,3 +171,18 @@ $('#total_price').text(total_price.toFixed(2));
 $('#com_wrap').on('click','.publish',function(){
   window.location.reload();
 })
+
+window.onload = function(){
+    var oTop = document.getElementById("to_top");
+    var screenw = document.documentElement.clientWidth || document.body.clientWidth;
+    var screenh = document.documentElement.clientHeight || document.body.clientHeight;
+    oTop.style.left = screenw - oTop.offsetWidth +"px";
+    oTop.style.top = screenh - oTop.offsetHeight + "px";
+    window.onscroll = function(){
+      var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+      oTop.style.top = screenh - oTop.offsetHeight + scrolltop +"px";
+    }
+    oTop.onclick = function(){
+      document.documentElement.scrollTop = document.body.scrollTop =0;
+    }
+  }  
