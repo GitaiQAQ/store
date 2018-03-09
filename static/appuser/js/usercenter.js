20b8db1370ca6bbb9a14db1a4e5f97577e519877
sivanZhang <445080336@qq.com>
2018-03-09 23:01:49 +0800

    样式完善

--- static/appuser/js/usercenter.js
+++ static/appuser/js/usercenter.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    /* 循环得出小计 */
     /* for(var i=0;i<$('.item').length;i++){
         var number =$('.price-sum').eq(i).parents('.item').find('.nub').text(),
             price = $('.price-sum').eq(i).parents('.item').find('.price').text(),
