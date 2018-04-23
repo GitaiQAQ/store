3d5489e5519cea39408ef3ee52521e232c448299
sivanZhang <445080336@qq.com>
2018-04-23 20:50:43 +0800

    增加网页

--- static/appuser/js/pay.js
+++ static/appuser/js/pay.js
@@ -1,3 +1,28 @@
-$('#back').click(function(){
-    window.location.href='/bill/bills/?unpayed';
+$('#back').click(function () {
+    window.location.href = '/bill/bills/?unpayed';
 })
+$('.qr-code').load(function () {
+    var inquireAbout = setInterval(inquireAbout(), 1000);
+    //请求服务器检查状态
+function inquireAbout() {
+    var billno = $('#billno').text();
+    $.ajax({
+        type: 'get',
+        url: '/pay/weixin/',
+        billno:'billno',
+        success: function (data) {
+            console.log(data);
+            if (data['status'] == 'ok') {
+                $().message(data['status']);
+                clearInterval(inquireAbout);//停止查询
+            } else {
+                $().errormessage(data['msg']);
+            }
+        },
+        error: function () {
+            $().errormessage(data['status']);
+        }
+    })
+}
+})
+
