cf9ff83b457c33729bcb1e9a03de60194f6e6075
sivanZhang <445080336@qq.com>
2018-04-29 01:16:57 +0800

    手机版JS

--- static/js/m_home.js
+++ static/js/m_home.js
@@ -1,18 +1,12 @@
-$('.submenu img').height($('.submenu img').width());
-$(".search").css("border-radius", $('.search').height() + 'px');
-$(".fa-search").css("line-height", $('.search').height() + 'px');
-//轮播图 宽高比
-$('#swiper-container').height($('#swiper-container').width() * 0.6 + 'px');
-
- var mySwiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next',
-    autoplay: 3000,
-    speed: 300,
-});
-var mySwiper2 = new Swiper('.swiper-container-2', {
-    pagination: '.swiper-pagination2',
-    prevButton: '.swiper-button-prev2',
-    nextButton: '.swiper-button-next2',
-});
+$(document).ready(function(){
+    var swiper = new Swiper('.swiper-container', {
+       pagination: '.swiper-pagination',
+       paginationClickable: true,
+       nextButton: '.swiper-button-next',
+       prevButton: '.swiper-button-prev',
+       // Enable debugger
+       debugger: true,
+       autoplay : 5000,
+       loop : true
+   });
+})
