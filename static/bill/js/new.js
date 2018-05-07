1335786dd435737b20d527ed3b76e3cc22d33537
sivanZhang <445080336@qq.com>
2018-05-07 12:23:02 +0800

    企业点不开

--- static/bill/js/new.js
+++ static/bill/js/new.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                 var codeVal = $(this).parent().next().text();
                 $('#coupon-number').val(codeVal);
             });
-            $('input[type="radio"]').iCheck({
+            $('.choose').iCheck({
                 checkboxClass: 'icheckbox_flat-red',
                 radioClass: 'iradio_flat-red',
                 increaseArea: '20%'
@@ -248,7 +248,7 @@ $(document).ready(function () {
             $('.invoice_company').hide('slow');
         }
     });
-    $('input[type="radio"]').iCheck({
+    $('.invoice_type').iCheck({
         checkboxClass: 'icheckbox_flat-red',
         radioClass: 'iradio_flat-red',
         increaseArea: '20%'
