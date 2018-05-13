cbbdd8cbce3194d0e6595f913e3ee8d489072b27
jeawy <461882709@qq.com>
2018-05-13 11:42:02 +0800

    购物车批量删除

--- shopcar/views.py
+++ shopcar/views.py
@@ -52,51 +52,59 @@ class ShopcarView(APIView):
             user = request.user
              
             if 'method' in request.POST:
-                method = request.POST['method']
-            
+                method = request.POST['method'] 
                 ruleid = request.POST['ruleid']
-                try:
-                    rule = AdaptorRule.objects.get(id = ruleid)
-            
-                    if method == 'delete': 
-                        try:
-                            caritem = CartItem.objects.get(rule = rule, user = user)
-                            caritem.delete()
-                        except CartItem.DoesNotExist:
-                            pass
-                        except CartItem.MultipleObjectsReturned:
-                            CartItem.objects.filter(rule = rule, user = user).delete()
-                            
+                
+                if ',' in ruleid:
+                    ruleids = ruleid.split(',')
+                    if method == 'delete':  
+                        caritem = CartItem.objects.filter(rule__id__in = ruleids, user = user)
+                        caritem.delete() 
                         result['status'] = 'ok'
                         result['msg']    = _('Delete successfully....')#'删除成功...' 
-                    else :
-                        # create 
-                        quantity = request.POST['num']
-                        desc = request.POST['desc']
-                        title = request.POST['title']
-                        car, create = CartItem.objects.get_or_create(rule = rule, user=user )
-                        if not create:
-                            car.quantity += int(quantity )
-                        else:
-                            car.quantity = int(quantity)
-                        if car.quantity > 300 :# 边界值300,最大购买数量
-                            car.quantity = 300
-                        # 如何数量大于库存的话，以库存为准 
-                        if  rule.inventory is not None:
-                            if rule.inventory == 0 :
-                                result['status'] = 'error'
-                                result['msg']    = '库存不足...' 
-                                return HttpResponse(json.dumps(result), content_type='application/json')
-                            elif rule.inventory < car.quantity :
-                                car.quantity = rule.inventory
-                        car.desc = desc
-                        car.title = title
-                        car.save()
-                        result['status'] = 'ok'
-                        result['msg']    = _('Add successfully....') # '添加成功...' 
-                except AdaptorRule.DoesNotExist:
-                    result['status'] = 'error'
-                    result['msg']    = _('Not found....') #'未找到商品....'
+                else:
+                    try:
+                        rule = AdaptorRule.objects.get(id = ruleid)
+                
+                        if method == 'delete': 
+                            try:
+                                caritem = CartItem.objects.get(rule = rule, user = user)
+                                caritem.delete()
+                            except CartItem.DoesNotExist:
+                                pass
+                            except CartItem.MultipleObjectsReturned:
+                                CartItem.objects.filter(rule = rule, user = user).delete()
+                                
+                            result['status'] = 'ok'
+                            result['msg']    = _('Delete successfully....')#'删除成功...' 
+                        else :
+                            # create 
+                            quantity = request.POST['num']
+                            desc = request.POST['desc']
+                            title = request.POST['title']
+                            car, create = CartItem.objects.get_or_create(rule = rule, user=user )
+                            if not create:
+                                car.quantity += int(quantity )
+                            else:
+                                car.quantity = int(quantity)
+                            if car.quantity > 300 :# 边界值300,最大购买数量
+                                car.quantity = 300
+                            # 如何数量大于库存的话，以库存为准 
+                            if  rule.inventory is not None:
+                                if rule.inventory == 0 :
+                                    result['status'] = 'error'
+                                    result['msg']    = '库存不足...' 
+                                    return HttpResponse(json.dumps(result), content_type='application/json')
+                                elif rule.inventory < car.quantity :
+                                    car.quantity = rule.inventory
+                            car.desc = desc
+                            car.title = title
+                            car.save()
+                            result['status'] = 'ok'
+                            result['msg']    = _('Add successfully....') # '添加成功...' 
+                    except AdaptorRule.DoesNotExist:
+                        result['status'] = 'error'
+                        result['msg']    = _('Not found....') #'未找到商品....'
             
             else:   
                 result['status'] = 'error'
